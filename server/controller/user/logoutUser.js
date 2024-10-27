2f9640a0f8004809dadc315d9e4157c41ea8caba
kabir <kabirehsan476@gamil.com>
2024-10-27 14:30:03 +0530

    modify logout function

--- server/controller/user/logoutUser.js
+++ server/controller/user/logoutUser.js
@@ -2,8 +2,14 @@ const TryCatch = require("../../utils/TryCatch.js");
 const ApiResponse = require("../../utils/ApiResponse.js");
 
 const logout = TryCatch(async (req, res, next) => {
-  res.clearCookie("accessToken");
-  res.clearCookie("refreshToken");
+  res.clearCookie("accessToken", { path: "/", sameSite: "None", secure: true });
+
+  res.clearCookie("refreshToken", {
+    path: "/",
+    sameSite: "None",
+    secure: true,
+  });
+
   res.status(200).json(new ApiResponse(200, "successfully logout"));
 });
 
